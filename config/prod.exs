3fe6d6f32966e40c2a0409b322d9bae1946c7154
Christophe <christophe.belpaire@gmail.com>
2015-09-22 15:52:06 +0200

    Heroku ready

--- config/prod.exs
+++ config/prod.exs
@@ -15,7 +15,7 @@ config :tron, Tron.Endpoint,
   http: [port: System.get_env("PORT")],
   url: [scheme: "https", host: "intense-reaches-7594.heroku.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  cache_static_manifest: "priv/static/manifest.json",
+  cache_static_manifest: "priv/static/manifest.json"
 
 # Do not print debug messages in production
 config :logger, level: :info
