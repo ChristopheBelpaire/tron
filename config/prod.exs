49690f6fc2ff1d408b40be88cc13cad704718ec6
Christophe <christophe.belpaire@gmail.com>
2015-09-22 15:42:48 +0200

    Heroku ready

--- config/prod.exs
+++ config/prod.exs
@@ -17,11 +17,14 @@ config :tron, Tron.Endpoint,
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE"),
+
+# Do not print debug messages in production
+config :logger, level: :info
+
+config :tron, Tron.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
   pool_size: 20
-# Do not print debug messages in production
-config :logger, level: :info
 
 # ## SSL Support
 #
